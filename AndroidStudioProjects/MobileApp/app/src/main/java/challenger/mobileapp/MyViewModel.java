334a7aed14a85b4a142b2ae88d7de9ca7ba8da3c
falloodaboy <40337476+falloodaboy@users.noreply.github.com>
2019-01-06 18:50:55 -0500

    New Changelist and Database Rules Test

--- AndroidStudioProjects/MobileApp/app/src/main/java/challenger/mobileapp/MyViewModel.java
+++ AndroidStudioProjects/MobileApp/app/src/main/java/challenger/mobileapp/MyViewModel.java
@@ -2,6 +2,10 @@ package challenger.mobileapp;
 
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -18,5 +22,11 @@ public class MyViewModel extends ViewModel {
         ref.setValue("Hello Again World!");
         ref2.setValue("Hello World");
         ref.child("texts").setValue("Text In a Message");
+        ref.child("users").setValue("This is the User's Message").addOnFailureListener(new OnFailureListener(){
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG, "onFailure: This has failed");
+            }
+        });
     }
 }
